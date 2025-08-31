86761c7eb5ffe1e8d2afe0729920137c05b12d2c
sabbirkhanparmar-oss <sabbirkhanparmar@gmail.com>
2025-08-31 23:30:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav a').forEach(link => {
+
+  link.addEventListener('click', e => {
+
+    e.preventDefault();
+
+    const target = document.querySelector(link.getAttribute('href'));
+
+    if(target) target.scrollIntoView({behavior:'smooth'});
+
+  });
+
+});
